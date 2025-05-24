a1656d816b6796bd9e6705f87457fde36f9d682f
elias pereira <eliaspinheiropereiraa@gmail.com>
2025-05-24 14:57:12 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/foodtechproject/foodtech/FoodtechApplication.java
@@ -0,0 +1,13 @@
+package br.com.foodtechproject.foodtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtechApplication.class, args);
+	}
+
+}
